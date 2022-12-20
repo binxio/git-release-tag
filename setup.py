0e8c102a7e85da2595d327b290dcb031a606f8b1
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-12-20 15:37:13 +0100

    bumped  to release 0.7.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="git-release-tag",
-    version="0.7.1",
+    version="0.7.2",
     url="https://github.com/binxio/git-release-tag",
     license="BSD",
     author="Mark van Holsteijn",
